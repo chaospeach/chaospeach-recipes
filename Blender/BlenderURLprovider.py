cfe432e7aa7ecdafd9436c48d77999d7c5863b7c
ashesamurai <me@ashe.wtf>
2024-01-24 19:44:53 -0500

    Added Blender download / munki recipes

--- Blender/BlenderURLprovider.py
+++ Blender/BlenderURLprovider.py
@@ -17,7 +17,6 @@ from autopkglib.URLGetter import URLGetter
 # https://www.blender.org/download/release/Blender4.0/blender-4.0.2-macos-x64.dmg/
 pre_mirror_url = 'https://www.blender.org/download/release/'
 os_str = 'macos'
-arch_str = ''
 
 archs = {
     'arm64': 'arm64',
@@ -31,13 +30,17 @@ class BlenderURLProvider( URLGetter ):
 
     # autopkg input
     input_variables = {
+        'arch' : {
+            'required': True,
+            'description': "Specify architecture of the the environment ('x86_64', or 'arm64'))"
+        },
         'base_version' : {
             'required': True,
             'description': "Base blender version (format: 'x.y')"
         },
-        'arch' : {
+        'mirror_override_url' : {
             'required': True,
-            'description': "Specify architecture of the the environment ('x86_64', or 'arm64'))"
+            'description': "Use the specified mirror URL (incl. trailing '/', exclude 'BlenderX.Y')"
         }
     }
 
@@ -48,43 +51,65 @@ class BlenderURLProvider( URLGetter ):
         },
         'download_version': {
             'description': "Version of jre/jdk to from the download_url"
+        },
+        'download_file': {
+            'description': "Full filename of download"
         }
     }
 
-
     def getDownloadURL( self ):
         # Combine mirror url with version specified from recipe
-        release_url = '%s/Blender%s/' % ( pre_mirror_url, self.env['base_version'] )
-        # Use the download feature of URLGetter
-        release_index_page = self.download(release_url, text=True)
-
+        release_url = '%sBlender%s/' % ( pre_mirror_url, self.env['base_version'] )
+        if '%s' % self.env['mirror_override_url']:
+            # We're overriding the mirror
+            release_override_url = ( '%sBlender%s/' % ( self.env['mirror_override_url'], self.env['base_version'] ) )
+            release_index_page = self.download( release_override_url, text=True )
+        else:
+            # Use the download feature of URLGetter
+            release_index_page = self.download( release_url, text=True )
+        
         # Time for regex
         # Grab all subversions available for our os / arch
         re_match_all_macos = re.compile(
-            'blender-%s.\d-%s-%s.dmg' % ( self.env['base_version'], os_str,  arch_str )
+            '\"blender-%s.\d-%s-%s\.dmg\"' % ( self.env['base_version'], os_str,  archs[self.env['arch']] )
         )
         matched_vers_for_arch = re.findall( re_match_all_macos, release_index_page )
+
         # Make sure we actually got something here
         if not matched_vers_for_arch:
-            raise ProcessorError( 'No versions found with the base version provided %s and arch of %s' % ( self.env['base_version'], arch_str ) )
-        # We've got something
-        matched_vers_for_arch = sorted( matched_vers_for_arch, key = lambda tup: tup[1], reverse = True )
+            raise ProcessorError( 'No versions found with the base version provided %s (%s)' % ( self.env['base_version'], archs[self.env['arch']] ) )
         
+        # We've got something, sort for latest (second entry when split by '-')
+        matched_vers_for_arch = sorted( matched_vers_for_arch, key = lambda s: s.split('-')[1], reverse = True )
+        self.output( 'Found %s versions for %s (%s)' % ( len(matched_vers_for_arch), self.env['base_version'], archs[self.env['arch']] ) )
+        matched_latest_vers_item = matched_vers_for_arch[0]
+        matched_latest_vers_dmg = matched_latest_vers_item.replace( '\"', '' )
+        matched_latest_vers = matched_latest_vers_dmg.split( '-' )[1]
+        self.env['download_file'] = matched_latest_vers_dmg
+        self.env['download_version'] = matched_latest_vers
+        self.output( 'Latest version of (%s) is %s.' % ( self.env['base_version'], matched_latest_vers ) )
 
-
+        # We have what we need, craft the URL
+        if '%s' % self.env['mirror_override_url']:
+            # Override if provided
+            latest_vers_url = self.env['mirror_override_url'] + 'Blender' + self.env['base_version'] + '/' + matched_latest_vers_dmg
+        else:
+            # Use Blender URL
+            latest_vers_url = pre_mirror_url + 'Blender' + self.env['base_version'] + '/' + matched_latest_vers_dmg
+        self.env['download_url'] = latest_vers_url
+        self.output( 'URL for %s (%s) is: %s' % ( matched_latest_vers_dmg[1], archs[self.env['arch']], latest_vers_url ) )
+        
     def main( self ):
         # Check for valid arch
-        if "%s" % self.env['arch'] in archs:
-            # Set arch_string for sugar
-            arch_str = '%s' % archs[self.env['arch']]
+        if '%s' % self.env['arch'] in archs:
             # Grab URL based on arch
-            self.output( 'Getting URL for Blender %s for %s (%s)', ( self.env['base_version'], os_str, self.env['arch'] ) )
+            self.output( 'Getting URL for Blender %s for %s (%s)' % ( self.env['base_version'], os_str, self.env['arch'] ) )
             try:
                 self.getDownloadURL()
             except Exception as err:
                 raise ProcessorError(err)
 
             
-if __name__ == "__main__":
+if __name__ == '__main__':
     PROCESSOR = BlenderURLProvider()
     PROCESSOR.execute_shell()
