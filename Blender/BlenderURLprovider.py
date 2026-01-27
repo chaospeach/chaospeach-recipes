b1ace4fe75d5417a0be2b9d8e3aee718b589149f
chaospeach <me@chaospea.ch>
2026-01-27 09:58:15 -0500

    Updated BlenderURLProvider to default to "download.blender.org"

--- Blender/BlenderURLprovider.py
+++ Blender/BlenderURLprovider.py
@@ -13,8 +13,8 @@ import re
 from autopkglib import ProcessorError
 from autopkglib.URLGetter import URLGetter
 
-# https://www.blender.org/download/release/Blender4.0/blender-4.0.2-macos-x64.dmg/
-pre_mirror_url = 'https://www.blender.org/download/release/'
+# https://download.blender.org/download/release/Blender4.0/blender-4.0.2-macos-x64.dmg/
+pre_mirror_url = 'https://download.blender.org/download/release/'
 os_str = 'macos'
 
 archs = {
@@ -38,7 +38,7 @@ class BlenderURLProvider( URLGetter ):
             'description': "Base blender version (format: 'x.y')"
         },
         'mirror_override_url' : {
-            'required': True,
+            'required': False,
             'description': "Use the specified mirror URL (incl. trailing '/', exclude 'BlenderX.Y')"
         }
     }
